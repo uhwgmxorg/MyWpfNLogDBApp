e0b9c5747ced9726febf3c933565116c7ce23823
Uwe Walter <uhw@gmx.org>
2023-01-02 19:47:13 +0100

    Bug fix in SetupDB.sql.

--- SQL/SetupDB.sql
+++ SQL/SetupDB.sql
@@ -15,7 +15,7 @@ BEGIN
 		Id bigint NOT NULL PRIMARY KEY IDENTITY(1,1),
 		CreatedOn nvarchar(max),
 		Level nvarchar(10),
-		AppUser int,
+		AppUser nvarchar(20),
 		Message nvarchar(max),
 		StackTrace nvarchar(max),
 		Exception nvarchar(max),
